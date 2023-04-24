ca8610260628f140b421780a5a5a126f3eb9108d
andrei162 <127126771+andrei162@users.noreply.github.com>
2023-04-24 20:42:31 +0300

    Update culoare.py

--- lib/culoare.py
+++ lib/culoare.py
@@ -1,6 +1,5 @@
 def gaseste_culoare_crin():
-    """
-    Exemplu - crchende
-    """
-    return "alb"
+    colors = ['rosu','portocaliu' 'galben', 'roz', 'mov', 'alb']
+    
+    return random.choice(colors)
 
