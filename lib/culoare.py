db82644c318f9186b242c2b505a72435ee6ab07b
Andrei Serban <whellie16@gmail.com>
2023-04-24 20:45:14 +0300

    adaugare def.culoare_random

--- lib/culoare.py
+++ lib/culoare.py
@@ -9,3 +9,7 @@ def gaseste_culoare_lalea():
     Exemplu - Andrei Murgoci
     """
     return "roz"
+    
+def gaseste_culoare_random():
+    colors = ['rosu', 'portocaliu', 'galben', 'roz', 'mov', 'alb']
+    return random.choice(colors)
